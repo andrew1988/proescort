4eafbd22ce5162bee42be06702103b67eb08ff1f
andrew1988 <mateikind@gmail.com>
2017-08-10 00:38:57 +0300

    started to format main navigation in header section, added borders and containers

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <title>Proescorts</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <!-- Styles -->
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,18 @@
 <header class="site-header">
   <nav class="main-navigation">
-    <a href="#">Link1</a>
-    <a href="#">Link2</a>
-    <a href="#">Link3</a>
+    <div class="row menu-item">
+      <div class="col-xs-2 col-half-offset"><a href="#">Link1</a></div>
+      <div class="col-xs-2 col-half-offset"><a href="#">Link1</a></div>
+      <div class="col-xs-2 col-half-offset"><a href="#">Link1</a></div>
+      <div class="col-xs-2 col-half-offset"><a href="#">Link1</a></div>
+      <div class="col-xs-2 col-half-offset"><a href="#">Link1</a></div>
+    </div>
   </nav>
   <section class="section-logo-site-signup">
-    [logo] - [signup] - [login]
+    ProEscorts - [signup] - [login]
   </section>
   <nav class="second-navigation">
     this one might be removes, will have to see about that.
   </nav>
-  
+
 </header>
