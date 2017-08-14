250a5415b6410684c5f74d2bfdcdb8fa47964606
andrew1988 <mateikind@gmail.com>
2017-08-15 00:19:37 +0300

    continued on header added logo and formaated site name

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,7 +9,34 @@
     </div>
   </nav>
   <section class="section-logo-site-signup">
-    <span>ProEscorts</span> - [signup] - [login]
+    <div class="row">
+        <div class="col-md-4">
+            <div class="row">
+              <div class="col-md-6">
+                <img src="/site-img/pro_escorts_logo.png" width="140">
+              </div>
+              <div class="col-md-6 format_logo">
+                  <span class="start_logo_name">Pro</span><span class="end_logo_name">Escorts</span>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="sign_in_form">
+                Cauti o escorta sau esti escorta?<br>
+                Inscriete-te acum in cel mai profesionist portal
+                de escorte din Romania!<br>
+                <a href="#">Inscriere</a>
+
+            </div>
+        </div>
+        <div class="col-md-4">Ai deja cont?<br>
+          Te poti loga aici:
+        {{--  {!! Form::open() !!}
+            {!! Form::text('email') !!}
+            {!! Form::password('password') !!}
+          {!! Form::close() !!} --}}
+        </div>
+    </div>
   </section>
   <nav class="second-navigation">
     this one might be removes, will have to see about that.
