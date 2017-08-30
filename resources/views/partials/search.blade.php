3daa65438034574afcb966f7651e394e291e75a5
andrew1988 <mateikind@gmail.com>
2017-08-30 23:23:15 +0300

    search bar progress

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,9 +1,5 @@
 <section class="section-search">
-  <div class="container">
-    <div class="row">
       <div class="main-search-body">
         [search main body]
       </div>
-    </div>
-  </div>
 </section>
