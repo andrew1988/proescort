b93999820d07ae795274e3c7094d377ca2aabf3f
andrew1988 <mateikind@gmail.com>
2017-09-01 00:34:00 +0300

    added basic search filters not styled yet

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,5 +1,21 @@
 <section class="section-search">
-      <div class="main-search-body">
-        [search main body]
-      </div>
+  {!!Form::open()!!}
+    <div class="row">
+        <div class="col-md-2 col-md-offset-1">
+          Judet:{!! Form::select("city",['0'=>'Alege Judet']) !!}
+        </div>
+        <div class="col-md-2">
+          Oras:{!! Form::select("city",['0'=>'Alege Oras']) !!}
+        </div>
+        <div class="col-md-2">
+          Online:{!! Form::checkbox('1') !!}
+        </div>
+        <div class="col-md-2">
+          Escorta sociala {!! Form::checkbox('1') !!}
+        </div>
+        <div class="col-md-2">
+          Cu video {!! Form::checkbox('1') !!}
+        </div>
+    </div>
+    {!! Form::close() !!}
 </section>
