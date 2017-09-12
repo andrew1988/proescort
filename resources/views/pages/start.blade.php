0dfb749506f666a868edf50829355560222524dd
andrew1988 <mateikind@gmail.com>
2017-09-12 23:51:03 +0300

    divided start page blade , no css yet

--- resources/views/pages/start.blade.php
+++ resources/views/pages/start.blade.php
@@ -1,4 +1,15 @@
 @extends("layouts.master")
 @section('content')
-
+  <section class="front-page-sponsored-video">
+      <div> promoted with video </div>
+  </section>
+  <section class="front-page-sponsored-normal">
+      <div>promoted normal</div>
+  </section>
+  <section class="front-page-listings">
+    <div class="row">
+        <div class="col-md-6">one</div>
+        <div class="col-md-6">two</div>
+    </div>
+  </section>
 @endsection
